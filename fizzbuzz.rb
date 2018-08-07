20db58cebe4f2cfa8dffa4a96b533f551a6db652
spethani <sarahpethani@gmail.com>
2018-08-07 17:43:54 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+def fizzbuzz(n)
+  if (n%3==0 && n%5==0)
+    puts "FizzBuzz"
+  elsif (n%3==0)
+    puts "Fizz"
+  elsif (n%5==0)
+    puts "Buzz"
+  end
+end
+
+fizzbuzz(3)
+fizzbuzz(5)
+fizzbuzz(15)
+fizzbuzz(4)
+  
